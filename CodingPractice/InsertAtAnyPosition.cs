65b6f0fc799857addd094393528a1e0b1a157fbe
Lenovo <Lenovo@DESKTOP-O8FHI11>
2025-01-15 23:53:33 +0530

    Add project files.

---
+++ CodingPractice/InsertAtAnyPosition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    public class InsertAtAnyPosition
+    {
+
+
+        public int[] InsertElementAtAnyPosition(int[] arr, int x, int pos) 
+        {
+            // {1, 2, 4, 6, 8, 0 }
+            int[] newarr = new int[arr.Length + 1];
+
+            for(int i = 0;i< newarr.Length - 1; i++)
+            {
+                if(i < pos)
+                {
+                    newarr[i] = arr[i];
+                }
+                else if(i == pos)
+                {
+                    newarr[i] = x;
+                }
+                else
+                {
+                    newarr[i] = arr[i - 1];
+                }
+
+            }
+            return newarr;       
+        }
+
+    }
+}
---
+++ CodingPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using CodingPractice;
+
+public class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        InsertAtAnyPosition IP = new InsertAtAnyPosition();
+
+        int[] arr = { 1, 2, 4, 6, 8, 0 };
+
+        int[] newarr = IP.InsertElementAtAnyPosition(arr,12,2);
+
+       foreach(int ar in arr) 
+        {
+            Console.Write("" + arr);
+        }
+
+
+    }
+}
