836d90dcff28009dbf8f6eda33c9a69a34cbc4c2
omkar.vichare <omkar.vichare@INQUIZITY>
2025-01-16 23:01:02 +0530

    Second largest

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -5,16 +5,33 @@ public class Program
     
     public static void Main(string[] args)
     {
-        InsertAtAnyPosition IP = new InsertAtAnyPosition();
+        #region Insertion of array
+        // InsertAtAnyPosition IP = new InsertAtAnyPosition();
 
-        int[] arr = { 1, 2, 4, 6, 8, 0 };
+        // int[] arr = { 1, 2, 4, 6, 8, 0 };
 
-        int[] newarr = IP.InsertElementAtAnyPosition(arr,12,2);
+        // int[] newarr = IP.InsertElementAtAnyPosition(arr,12,2);
 
-       foreach(int ar in arr) 
-        {
-            Console.Write("" + arr);
-        }
+        //foreach(int ar in arr) 
+        // {
+        //     Console.Write("" + arr);
+        // }
+        #endregion
+
+
+
+        #region Second Largest Element
+
+        SecondLargestElement  sc = new SecondLargestElement();
+
+        int[] arr = { 1, 2, 32, 4, 26, 17, 8 };
+
+        int Element = sc.SecondLargest(arr);
+
+        Console.WriteLine(Element);
+
+        #endregion
+
 
 
     }
---
+++ CodingPractice/SecondLargestElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    public class SecondLargestElement
+    {
+
+        public int SecondLargest(int[] arr)
+        {
+
+            int secondlargest = 0;
+
+
+            int laregstElement = 0;
+
+            for (int i = 0; i <= arr.Length - 1; i++) 
+            {
+                if (arr[i] > laregstElement)
+                {
+                    secondlargest = laregstElement;
+                    laregstElement = arr[i];
+                }
+                if (arr[i] > secondlargest && arr[i]!= laregstElement)
+                {
+                    secondlargest = arr[i];
+                }
+
+            }
+         
+
+            return secondlargest;
+
+        }
+
+
+
+    }
+}
